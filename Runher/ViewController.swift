a69ecd6d6f11dced2109b042b6838a2a731b20b2
Hfriedman13 <hfriedman13@gmail.com>
2020-12-28 13:14:22 -0500

    implemented allowing location while in use

--- Runher/ViewController.swift
+++ Runher/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
     }
+    
+    
+    
+    @IBAction func startPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func statsPressed(_ sender: UIButton) {
+    }
+    
 
-
+    @IBAction func settingsPressed(_ sender: UIButton) {
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
 
